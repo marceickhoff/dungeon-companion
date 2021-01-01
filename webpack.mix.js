9bb775cca588fe7710a4e28d330a11de4aa5e675
marceickhoff <mail@marceickhoff.com>
2021-01-01 18:34:34 +0100

    Initial

---
+++ webpack.mix.js
@@ -0,0 +1,57 @@
+// Laravel Mix
+const mix = require('laravel-mix');
+
+// Laravel Mix extensions
+require('laravel-mix-clean-css');
+require('laravel-mix-modernizr');
+
+// Public path
+mix.setPublicPath('public');
+
+// Files
+mix.sass('src/scss/app.scss', 'css/app.css');
+mix.js([
+	'src/js/app.js',
+	'node_modules/@fortawesome/fontawesome-pro/js/solid.js',
+	'node_modules/@fortawesome/fontawesome-pro/js/fontawesome.js'
+], 'js/app.js').extract();
+
+// Additional PostCSS plugins
+mix.options({
+	postCss: [
+		require('mqpacker')({ sort: true })
+	]
+});
+
+// clean-css
+mix.cleanCss({
+	level: {
+		2: {
+			all: true
+		}
+	},
+	format: {
+		breaks: {
+			afterAtRule: true,
+			afterBlockBegins: true,
+			afterBlockEnds: true,
+			afterComment: true,
+			afterProperty: true,
+			afterRuleBegins: true,
+			afterRuleEnds: true,
+			beforeBlockEnds: true,
+			betweenSelectors: true
+		},
+		indentBy: 1,
+		indentWith: 'tab', // The only proper way, fight me irl
+		spaces: {
+			aroundSelectorRelation: true,
+			beforeBlockBegins: true,
+			beforeValue: true
+		},
+		semicolonAfterLastProperty: true
+	}
+});
+
+// Modernizr
+mix.modernizr();
